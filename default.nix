d93c594a84253a4322b398694462c800ef57a282
Peter Jones <pjones@devalot.com>
2020-04-23 17:10:44 -0700

    Update build system

--- default.nix
+++ default.nix
@@ -1,19 +1,18 @@
-{ pkgs ? import <nixpkgs> { }
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs {}
+, nix-hs ? import sources.nix-hs { inherit pkgs; }
+, ghcide ? sources.ghcide-nix
+, ormolu ? sources.ormolu
 }:
 
-let
-  # Helpful if you want to override any Haskell packages:
-  overrides = self: super: {
-    http-client = if super ? http-client_0_6_2
-      then super.http-client_0_6_2
-      else super.http-client;
+nix-hs {
+  cabal = ./themoviedb.cabal;
+  overrides = lib: self: super: with lib; {
+    ghcide = import ghcide {};
+
+    ormolu = (import ormolu {
+      inherit (lib) pkgs;
+      ormoluCompiler = lib.compilerName;
+    }).ormolu;
   };
-
-  # Apply the overrides from above:
-  haskell = pkgs.haskellPackages.override (orig: {
-    overrides = pkgs.lib.composeExtensions
-      (orig.overrides or (_: _: {})) overrides; });
-in
-
-# Load the local nix file and use the overrides from above:
-haskell.callPackage ./themoviedb.nix { }
+}
